8b480b6d4fb11ce0bbc05c4236b2fcae1180f8ae
mark_story <mark@mark-story.com>
2012-04-08 21:53:56 -0400

    Die named parameters die.
    Remove named parameters.  They were a terrible idea that hasn't worked
    out well.  Structring key value data in path segments
    essentially duplicating query string params doesn't follow web standards
    conventions, and the feature required a pile of configuration.

--- lib/Cake/Config/routes.php
+++ lib/Cake/Config/routes.php
@@ -16,9 +16,9 @@
  * @since         CakePHP(tm) v 2.0
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-use Cake\Routing\Router,
-	Cake\Core\Plugin,
-	Cake\Utility\Inflector;
+use Cake\Routing\Router;
+use Cake\Core\Plugin;
+use Cake\Utility\Inflector;
 
 /**
  * Connects the default, built-in routes, including prefix and plugin routes. The following routes are created
@@ -79,10 +79,5 @@ foreach ($prefixes as $prefix) {
 Router::connect('/:controller', array('action' => 'index'));
 Router::connect('/:controller/:action/*');
 
-$namedConfig = Router::namedConfig();
-if ($namedConfig['rules'] === false) {
-	Router::connectNamed(true);
-}
-
-unset($namedConfig, $params, $indexParams, $prefix, $prefixes, $shortParams, $match,
+unset($params, $indexParams, $prefix, $prefixes, $shortParams, $match,
 	$pluginPattern, $plugins, $key, $value);
